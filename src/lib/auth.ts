3170ddf06bbf709347f550f0be945401af0e3f02
ageuxo <ageuxo@gmail.com>
2025-05-03 13:03:24 +0200

    start setting up database for auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,18 @@
+import { betterAuth } from "better-auth";
+import { MysqlDialect } from "kysely";
+import { createPool } from "mysql2";
 
 
-export const auth = ({
-    
+const dialect = new MysqlDialect({
+  pool: createPool({
+    database: "devdepot",
+    host: "localhost"
+  })
+})
+
+export const auth = betterAuth({
+    database: {
+      dialect,
+      type: "mysql"
+    }
 })
