9dadd42b9bd8466dc3743c1793a0b0daf35405a1
ageuxo <ageuxo@gmail.com>
2025-05-04 12:33:34 +0200

    Change anchor tag into Link tag

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="flex items-center justify-between p-4 bg-gray-800 text-white">
-        <a href="/" className="text-2xl font-bold">
+        <Link href="/" className="text-2xl font-bold">
             <h1 className="text-2xl">Projects</h1>
-        </a>
+        </Link>
     </header>
   );
 }
