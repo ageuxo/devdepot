89cdc283b4b119a65d9916039fa6795b501a813a
ageuxo <ageuxo@gmail.com>
2025-05-04 12:34:08 +0200

    Make projects use Project class

--- src/app/projects.tsx
+++ src/app/projects.tsx
@@ -1,5 +1,17 @@
 
-export function ProjectList({ projects }: { projects: any[] }) {
+
+class Project {
+  name: string;
+  link: string;
+  description: string;
+  constructor(name: string, link: string, description: string) {
+    this.name = name;
+    this.link = link;
+    this.description = description;
+  }
+}
+
+export function ProjectList({ projects }: { projects: Project[] }) {
   const entries = projects.map((project) => (
     < ProjectCard key={project.name} project={project} />
   ));
@@ -11,12 +23,12 @@ export function ProjectList({ projects }: { projects: any[] }) {
   );
 }
 
-function ProjectCard({ project }: { project: any }) {
+function ProjectCard({ project }: { project: Project }) {
   return (
     <div className="p-4 border rounded shadow">
       <h2 className="text-xl font-bold">{project.name}</h2>
       <p>{project.description}</p>
-      <a href={project.link} className="text-blue-500">
+      <a href={project.link} className="text-blue-500" target="_blank">
         View Project
       </a>
     </div>
