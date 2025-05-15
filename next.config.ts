8f13a521e75d9d24709cf0bbe7ca4d541a5d84c9
ageuxo <ageuxo@gmail.com>
2025-05-15 11:51:03 +0200

    Add dev address env variable

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['localhost', process.env.REMOTE_DEV_ADDR!]
 };
 
 export default nextConfig;
