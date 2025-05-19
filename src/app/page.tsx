2c2edfc121f474faa7a82ce323a7317465a665d6
ageuxo <ageuxo@gmail.com>
2025-05-19 15:40:27 +0200

    Remove leftover test data in Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
 import { ProjectList } from "../components/projects";
 
-const projects = [
-  {
-    name: "Project 1",
-    description: "This is the first project",
-    link: "https://example.com/project1",
-  },
-  {
-    name: "Project 2",
-    description: "This is the second project",
-    link: "https://example.com/project2",
-  },
-];
-
 export default function Home() { // Make this use database
   return (
     <>
