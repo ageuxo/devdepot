b3528c56a238c69fc58964570ad4696cfdde7a98
ageuxo <ageuxo@gmail.com>
2025-06-18 10:54:20 +0200

    Added placeholder logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import AccountTab from "./account";
 import styles from "./header.module.css"
+import Image from "next/image";
+import logo from './logo.png'
 
 export default function Header() {
   return (
     <header className={styles.header}>
+        <Image src={logo} alt="DevDepot logo" width={25} height={25} />
         <Link href="/" className="">
             <h1 className={styles.headerText}>Projects</h1>
         </Link>
